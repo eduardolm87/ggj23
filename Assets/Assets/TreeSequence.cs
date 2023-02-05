d01516ab742126d5129f42564a2825c34fa9ffe2
edmunera <eduardo.munera@decagames.com>
2023-02-05 14:36:45 +0100

    treesequence

--- Assets/Assets/TreeSequence.cs
+++ Assets/Assets/TreeSequence.cs
@@ -8,21 +8,41 @@ using System.Linq;
 public class TreeSequence : MonoBehaviour
 {
     public GameObject TreeScenario;
+    public GameObject PlayerPivot;    
+    public List<GameObject> UIObjects = new List<GameObject>();
+
+    List<Transform> _transforms = new List<Transform>();
 
 
     void Awake()
     {
         TreeScenario.SetActive(false);
+        _transforms = TreeScenario.GetComponentsInChildren<Transform>().ToList();
+        Debug.Log("tengo "+_transforms.Count+" hijos");
     }
 
-    public void TreeSequenceStart(string zNodeName) {
-        
+    public void TreeSequenceStart() 
+    {
         TreeScenario.SetActive(true);
+        UIObjects.ForEach(x=>{
+            x.SetActive(false);
+        });
+        Invoke("TreeSequenceEnd",4f);
+    }
+
+    IEnumerator Timer(float seconds) 
+    {
+        yield return new WaitForSeconds(seconds);
+        TreeSequenceEnd();
     }
 
 
     void TreeSequenceEnd()
     {
+        UIObjects.ForEach(x=>{
+            x.SetActive(true);
+        });
+        TreeScenario.SetActive(false);
         Sequencer.Message("TreeSeqDone");
     }
     
