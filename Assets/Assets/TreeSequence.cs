f6eee13873e68a525049c867ed7e09421636982a
edmunera <eduardo.munera@decagames.com>
2023-02-05 17:03:50 +0100

    final

--- Assets/Assets/TreeSequence.cs
+++ Assets/Assets/TreeSequence.cs
@@ -10,28 +10,45 @@ public class TreeSequence : MonoBehaviour
     public GameObject TreeScenario;
     public GameObject TreeCamera;
     public List<GameObject> UIObjects = new List<GameObject>();
+    Vector3 originalPosition;
 
     List<Transform> _transforms = new List<Transform>();
 
+    public Transform _target = null;
+
+    public float MoveSpeed = 1f;
 
     void Awake()
     {
-       // TreeScenario.SetActive(false);
+        originalPosition = TreeCamera.transform.position;
+       TreeScenario.SetActive(false);
         _transforms = TreeScenario.GetComponentsInChildren<Transform>().ToList();
     }
 
     public void TreeSequenceStart(string zNode) 
-    {
+    {    
         TreeScenario.SetActive(true);
         UIObjects.ForEach(x=>{
             x.SetActive(false);
         });
-        Invoke("TreeSequenceEnd",0.1f);
+        
 
-        var target = _transforms.FirstOrDefault(x=>x.name == zNode);
-        Debug.Log("me voy a nodo "+target.name+" que esta en pos "+target.position);
+       _target = _transforms.FirstOrDefault(x=>x.name == zNode);      
+        Invoke("TreeSequenceEnd",5f);
+  
+    }
 
 
+    public void ResetCamera()
+    {
+        _target = null;
+    }
+
+    void Update()
+    {
+        if(_target==null) return;
+
+        TreeCamera.transform.position = Vector3.Lerp(TreeCamera.transform.position,_target.position,Time.deltaTime*MoveSpeed);        
     }
 
     
@@ -43,6 +60,8 @@ public class TreeSequence : MonoBehaviour
             x.SetActive(true);
         });
         TreeScenario.SetActive(false);
+        _target = null;
+        ResetCamera();
         Sequencer.Message("TreeSeqDone");
     }
 
